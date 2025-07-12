e37c1cf8ad7186be8c90e175c1b00937f897b044
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2025-07-12 17:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // prevent page refresh
+
+      const first = document.getElementById("firstName").value;
+      const last = document.getElementById("lastName").value;
+      const phone = document.getElementById("phoneNumber").value;
+      const email = document.getElementById("emailId").value;
+
+      const message = `First Name: ${first} Last Name: ${last} Phone Number: ${phone} Email ID: ${email}`;
+      alert(message);
+    });
